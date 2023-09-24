ff77d965ecd63be9f4c9ec1b898c1c28456afaa6
JocelynVelarde <a01285780@itesm.mx>
2023-09-24 06:31:12 -0600

    solved conflicts with Link statement

--- app/components/grid.tsx
+++ app/components/grid.tsx
@@ -13,7 +13,6 @@ const SearchCard: React.FC<SearchCardProps> = ({ title, description }) => {
     <div className="bg-base-200 shadow-lg p-4 rounded-md transition duration-300 transform hover:scale-105">
       <h3 className="text-lg font-semibold">{title}</h3>
       <p className="text-base-content">{description}</p>
-      <Link to="routes/_dashboard">Go to Dashboard</Link> 
     </div>
   );
 };
